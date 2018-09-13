e89118d5e81e1a4d9bf9e3d86ec35994dd201f07
Rob Phoenix <rob@robphoenix.com>
2018-09-13 19:29:55 +0100

    Add summary component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IStock } from './services/IStock';
+import { StocksService } from './services/stocks.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stocks';
+  stocks: IStock[];
+
+  constructor(private stocksService: StocksService) {
+    stocksService.load(['AAPL']).subscribe(stocks => {
+      this.stocks = stocks;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './components/summary/summary.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SummaryComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
